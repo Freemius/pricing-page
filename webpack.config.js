dba54e1976f3a935b97be9151a772076cd1df066
Swashata Ghosh <swas@freemius.com>
2022-03-16 16:31:57 +0530

    [auto-format] Setup prettier to auto-format before commit.

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,76 @@
 const path = require('path');
-const { ESBuildMinifyPlugin } = require('esbuild-loader')
 
 /**
  * Targeting `>0.1%, not dead`.
  *
  * {@link https://browserslist.dev/?q=PjAuMSUsIG5vdCBkZWFk}
  */
-const targetBrowsers = ['chrome79','firefox95','safari13.1','edge96'];
+const targetBrowsers = ['chrome79', 'firefox95', 'safari13.1', 'edge96'];
 
-module.exports = () =>  {
-    const buildMode = process.env.NODE_ENV;
-    const isProductionMode = buildMode === 'production';
+module.exports = () => {
+  const buildMode = process.env.NODE_ENV;
+  const isProductionMode = buildMode === 'production';
 
-    return {
-        mode: buildMode,
-        entry: './src/index.js',
-        devtool: isProductionMode ? false : 'eval-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'esbuild-loader',
-                            options: {
-                                loader: 'jsx',
-                                target: targetBrowsers,
-                            }
-                        }
-                    ],
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        "style-loader", "css-loader", {
-                            loader: 'esbuild-loader',
-                            options: {
-                                loader: 'css',
-                                minify: isProductionMode
-                            },
-                        }, "sass-loader"
-                    ]
-                },
-                {
-                    test: /\.(svg|png)/,
-                    use: [
-                        "file-loader"
-                    ]
-                }
-            ]
+  return {
+    mode: buildMode,
+    entry: './src/index.js',
+    devtool: isProductionMode ? false : 'eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'esbuild-loader',
+              options: {
+                loader: 'jsx',
+                target: targetBrowsers,
+              },
+            },
+          ],
         },
-        output: {
-            path: path.join(__dirname, './dist'),
-            filename: 'freemius-pricing.js',
-            library: ["Freemius"],
-            libraryTarget: 'umd',
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'esbuild-loader',
+              options: {
+                loader: 'css',
+                minify: isProductionMode,
+              },
+            },
+            'sass-loader',
+          ],
         },
-        // We cannot use ESBuild mimizer because our code-base isn't strictly ES Module.
-        // We still have some side-effecty imports.
-        // optimization: isProductionMode ?
-        // {
-        //     minimize: isProductionMode,
-        //     nodeEnv: buildMode,
-        //     minimizer: [new ESBuildMinifyPlugin({
-        //         target: targetBrowsers
-        //     })]
-        // } :
-        // undefined,
-        optimization: {
-            nodeEnv: buildMode,
-            minimize: buildMode === 'production',
-        }
-    };
+        {
+          test: /\.(svg|png)/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, './dist'),
+      filename: 'freemius-pricing.js',
+      library: ['Freemius'],
+      libraryTarget: 'umd',
+    },
+    // We cannot use ESBuild mimizer because our code-base isn't strictly ES Module.
+    // We still have some side-effecty imports.
+    // optimization: isProductionMode ?
+    // {
+    //     minimize: isProductionMode,
+    //     nodeEnv: buildMode,
+    //     minimizer: [new ESBuildMinifyPlugin({
+    //         target: targetBrowsers
+    //     })]
+    // } :
+    // undefined,
+    optimization: {
+      nodeEnv: buildMode,
+      minimize: buildMode === 'production',
+    },
+  };
 };
